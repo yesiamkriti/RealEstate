d76d7e8fb9e3e8c51afaa96fdb006950410aa3e3
Kriti <114799895+yesiamkriti@users.noreply.github.com>
2024-04-15 22:56:24 +0530

    Delete python_project_group3_Wizz-kid.py

--- python_project_group3_Wizz-kid.py
+++
@@ -1,5 +0,0 @@
-EmailId = str(input("Enter Your Email_Id: "))
-username = EmailId[:EmailId.index("@")]
-print("Your Name is",username ,end=(""))
-domain = EmailId[EmailId.index("@") + 1:]
-print("\nYour Domain is",domain.upper())
